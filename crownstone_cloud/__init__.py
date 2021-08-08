6c49cefeb8a8aabd3426d569f0e3d0866ac5bacf
Ricardo Steijn <ricardo.steijn97@gmail.com>
2021-08-08 17:40:16 +0200

    Bump version to 1.4.6

--- crownstone_cloud/__init__.py
+++ crownstone_cloud/__init__.py
@@ -2,3 +2,5 @@
 from crownstone_cloud.cloud import CrownstoneCloud
 from crownstone_cloud.helpers.aiohttp_client import create_clientsession
 from crownstone_cloud.util.runners import run_async
+
+__version__ = "1.4.6-git"
