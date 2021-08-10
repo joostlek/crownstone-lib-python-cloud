8be26d0375bec77338685297637f0b5e8a30caaa
vliedel <bart@crownstone.rocks>
2021-08-10 13:45:36 +0200

    Mark non-release again.

--- crownstone_cloud/__init__.py
+++ crownstone_cloud/__init__.py
@@ -3,4 +3,4 @@ from crownstone_cloud.cloud import CrownstoneCloud
 from crownstone_cloud.helpers.aiohttp_client import create_clientsession
 from crownstone_cloud.util.runners import run_async
 
-__version__ = "1.4.6"
+__version__ = "1.4.6-git"
